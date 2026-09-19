c3c05482af6efffc612bb60be5c099c88a9539f1
Juan Pablo <lazzyspin888@gmail.com>
2026-09-19 02:07:42 -0300

    create install sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install -y \
+  build-essential pkg-config git curl ca-certificates \
+  libwayland-dev libxkbcommon-dev libxkbcommon-x11-dev \
+  libglvnd-dev libegl-dev libgles-dev libgl-dev \
+  libudev-dev libinput-dev libseat-dev libdrm-dev libgbm-dev \
+  libdbus-1-dev libsystemd-dev \
+  libx11-dev libxcursor-dev libxrandr-dev libxi-dev libxcb1-dev \
+  libgl1-mesa-dri mesa-utils wayland-utils weston foot
